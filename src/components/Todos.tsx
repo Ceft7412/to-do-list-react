5725b3d7231d533d88044d7916340cc42ee1e885
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-14 20:50:17 +0800

    @update to handle modal

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,23 +1,41 @@
 import React, { useEffect, useState } from "react";
+import Confirmation from "./Modal/Confirmation";
+import Edit from "./Modal/Edit";
 
-function Todos() {
+function Todos({ lists, setLists }) {
   const [fetching, setFetching] = useState<boolean>(true);
-  const [lists, setLists] = useState<string[]>([]);
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const todos: string[] = JSON.parse(localStorage.getItem("todos") || "[]");
-      setLists(todos);
-      setFetching(false);
-    }
-  }, []);
-  if (fetching) {
-    return;
-  }
+  const [list, setList] = useState<number>();
 
+  const [modal, setModal] = useState<boolean>(false);
+  const [editModal, setEditModal] = useState<boolean>(false);
+  console.log("modal: ", modal);
+
+  const handleDeleteClick = (index: number) => {
+    setList(index);
+    setModal(true);
+  };
+
+  const handleEditClick = (index: number) => {
+    setEditModal(true);
+    setList(index);
+  };
   return (
     <>
       {lists.map((list, index) => (
-        <div key={index}>{list}</div>
+        <div key={index} className="todo-list">
+          {editModal && <Edit listToEdit={list} lists={lists} />}
+          <span className="todo-list__title">{list}</span>
+          <div className="todo-list__actions">
+            <button onClick={() => handleEditClick(index)}>Edit</button>
+
+            <button type="button" onClick={() => handleDeleteClick(index)}>
+              Delete
+            </button>
+            {modal && (
+              <Confirmation setModal={setModal} listToDelete={list} setLists={setLists} />
+            )}
+          </div>
+        </div>
       ))}
     </>
   );
