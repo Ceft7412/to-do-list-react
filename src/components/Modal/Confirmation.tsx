b3dbb244dc855a93531c4034e138b5d7040eebca
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-15 07:25:09 +0800

    @update Filter those not the same as the list selected.

--- src/components/Modal/Confirmation.tsx
+++ src/components/Modal/Confirmation.tsx
@@ -1,27 +1,28 @@
 import React from "react";
 
 function Confirmation({ setModal, listToDelete, setLists }) {
+  console.log("listToDelete: ", listToDelete);
   const handleDelete = () => {
     if (typeof window !== "undefined") {
       const todos: string[] = JSON.parse(localStorage.getItem("todos") || "[]");
-      const updatedTodos = todos.filter((_, index) => index !== listToDelete);
+      const updatedTodos = todos.filter((todo) => todo !== listToDelete);
       console.log("updatedTodos: ", updatedTodos);
       localStorage.setItem("todos", JSON.stringify(updatedTodos));
       setLists(updatedTodos);
-      setModal(false);
+      setModal(null);
     }
   };
   return (
     <div className="background-modal">
       <div className="modal">
         <div className="modal__header">
-          <div className="modal__exit" onClick={() => setModal(false)}>
+          <div className="modal__exit" onClick={() => setModal(null)}>
             <span>X</span>
           </div>
         </div>
         <div className="modal__body">Are you sure you want to delete this to-do?</div>
         <div className="modal__footer">
-          <button className="modal__action" type="button" onClick={() => setModal(false)}>
+          <button className="modal__action" type="button" onClick={() => setModal(null)}>
             Cancel
           </button>
           <button className="modal__action" onClick={handleDelete}>
