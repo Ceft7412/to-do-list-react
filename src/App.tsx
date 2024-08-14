f88e0b0c2d8775fea7238348824e4676899e19ff
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-14 20:35:23 +0800

    @update Effects to pass value to props

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { useState, useEffect } from "react";
 import Form from "./components/Form";
 import Todos from "./components/Todos";
 
 function App() {
+  const [lists, setLists] = useState<string[]>([]);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const todos: string[] = JSON.parse(localStorage.getItem("todos") || "[]");
+      setLists(todos);
+    }
+  }, []);
   return (
     <>
       <main className="container">
         <section className="item item1">
-          <Form />
+          <Form setLists={setLists} />
         </section>
         <section className="item item2">
           <h2>To-do List</h2>
-          <Todos />
+          <Todos lists={lists} setLists={setLists} />
         </section>
       </main>
     </>
